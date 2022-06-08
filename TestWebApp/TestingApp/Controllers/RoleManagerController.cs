1194f9638896d802f2cd95db86cb47f5eb9ada81
HaunBa <haunschmied.b@gmail.com>
2022-06-08 14:46:53 +0200

    Added Editing of Roles

--- TestWebApp/TestingApp/Controllers/RoleManagerController.cs
+++ TestWebApp/TestingApp/Controllers/RoleManagerController.cs
@@ -28,13 +28,61 @@ namespace TestingApp.Controllers
             if (roleName != null)
             {
                 await _roleManager.CreateAsync(new IdentityRole(roleName.Trim()));
-                _logger.LogInformation("Role Created");
+                _logger.LogInformation($"Role {roleName} Created");
             }
             else
             {
                 _logger.LogError("Rolename null");
             }
             return RedirectToAction("Index");
+        }
+        
+        public async Task<IActionResult> DeleteRole(string id)
+        {
+            var role = await _roleManager.FindByIdAsync(id);
+            if (role != null)
+            {
+                await _roleManager.DeleteAsync(role);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Edit(string? id)
+        {
+            var role = await _roleManager.FindByIdAsync(id);
+            if (role != null)
+            {
+                return View(role);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("Id,Name,NormalizedName,ConcurrencyStamp")] IdentityRole role)
+        {
+            if (id != role.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                var frole = await _roleManager.FindByIdAsync(role.Id);
+                if (frole.Name != role.Name)
+                {
+                    frole.Name = role.Name;
+                    
+                    var res = await _roleManager.UpdateAsync(role);
+                }
+                
+                return RedirectToAction(nameof(Index));
+            }
+            return View(role);
         }
     }
 }
