afe5f4094b8076e36130469766394e073074ca33
HaunBa <haunschmied.b@gmail.com>
2022-09-06 11:22:42 +0200

    Removed Test Project

--- TestWebApp/SmartVendor-Client/global-usings.cs
+++
@@ -1,6 +0,0 @@
-﻿global using System;
-global using System.Device.Gpio;
-global using System.Threading;
-global using Microsoft.AspNetCore.SignalR.Client;
-global using Microsoft.Extensions.Configuration;
-global using Microsoft.Extensions.Hosting;
