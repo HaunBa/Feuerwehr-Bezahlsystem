414140388d7dc8ab25abffb3e8aa0e6c4b5ece7b
HaunBa <haunschmied.b@gmail.com>
2022-09-15 15:31:12 +0200

    Update Program.cs

--- TestWebApp/GitHubUpdater/Program.cs
+++ TestWebApp/GitHubUpdater/Program.cs
@@ -1,4 +1,30 @@
 ﻿using GitHubUpdater;
+using System.Diagnostics;
 
-GitUpdater GitUpdater = new GitUpdater();
-await GitUpdater.Start();
+//GitUpdater GitUpdater = new GitUpdater();
+//await GitUpdater.Start();
+
+var thisProcess = Process.GetCurrentProcess();
+
+var process = new Process{
+        StartInfo = new ProcessStartInfo
+        {
+            FileName = typeof(Program).Assembly.GetName().Name+".exe",
+            UseShellExecute = false,
+            RedirectStandardOutput = true,
+            CreateNoWindow = false
+        }
+    };
+
+var processName = process.StartInfo.FileName;
+
+process.Start();
+
+Console.WriteLine("other process started");
+
+Thread.Sleep(1000);
+
+Console.WriteLine("Killing this Process...");
+thisProcess.Kill();
+
+Console.WriteLine("Killed?");
